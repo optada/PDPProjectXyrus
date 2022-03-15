4ab919193a3c5e4cbdd6b94fce8988af11127f3c
OPTada <druzhinin_v_d@ukr.net>
2022-03-15 11:03:34 +0200

    added base game instance

---
+++ Source/ProjectXyrus/Global/X_GameInstanceXyrusBase.h
@@ -0,0 +1,17 @@
+// Copyright Druzhynin Volodymyr (optada), All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "X_GameInstanceXyrusBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTXYRUS_API UX_GameInstanceXyrusBase : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};

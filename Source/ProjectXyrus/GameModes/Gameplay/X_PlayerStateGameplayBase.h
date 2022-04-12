a9ebaf58c6996556bc2703448593cc78a0f56d71
Druzhinin <v.druzhinin@pinglestudio.com>
2022-04-12 20:49:25 +0300

    PlayerStateGameplay base added HexCluster linking

--- Source/ProjectXyrus/GameModes/Gameplay/X_PlayerStateGameplayBase.h
+++ Source/ProjectXyrus/GameModes/Gameplay/X_PlayerStateGameplayBase.h
@@ -4,14 +4,23 @@
 
 #include "CoreMinimal.h"
 #include "ProjectXyrus/GameModes/X_PlayerStateBase.h"
+
 #include "X_PlayerStateGameplayBase.generated.h"
 
-/**
- * 
- */
+
+class AX_HexClusterBase;
+
+
 UCLASS()
 class PROJECTXYRUS_API AX_PlayerStateGameplayBase : public AX_PlayerStateBase
 {
 	GENERATED_BODY()
-	
+
+protected:
+	UPROPERTY(Replicated)
+	AX_HexClusterBase* MyHexCluster = nullptr;
+
+public:
+	void SetNewHexCluster(AX_HexClusterBase* NewHexCluster);
+	AX_HexClusterBase* GetMyHexCluster() const;
 };
